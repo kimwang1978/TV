ef91e51bb7e5c7a7b2f27ffe6d7ddc593c16cc22
kimwang1978 <kim.wang1978@gmail.com>
2024-04-25 15:11:19 +0900

    Add files via upload

---
+++ user_config.py
@@ -0,0 +1,31 @@
+source_file = "user_demo.txt"
+final_file = "user_result.txt"
+favorite_list = [
+    "CCTV-5",
+    "CCTV-5+",
+    "CCTV-13",
+    
+    "东方卫视",
+    "江苏卫视",
+    "浙江卫视",
+    "湖南卫视",
+    "新闻综合",
+    "上海都市",
+    "第一财经",
+    "上海ICS",
+    "上海娱乐",
+    "上海教育",
+    "东方影视",
+    "上海纪实",
+    "五星体育",
+    "东方财经",
+]
+favorite_page_num = 6
+default_page_num = 4
+urls_limit = 15
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "all"
+domain_blacklist = ["epg.pw"]
+url_keywords_blacklist = []
