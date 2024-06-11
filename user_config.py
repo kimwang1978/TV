d8e2b8858f31bd42f159b83267a38be15ea11853
WANG <kim.wang1978@gmail.com>
2024-06-11 11:39:39 +0900

    Update user_config.py

--- user_config.py
+++ user_config.py
@@ -38,11 +38,11 @@ recent_days = 30
 ipv_type = "ipv4"
 domain_blacklist = ["epg.pw"]
 url_keywords_blacklist = []
-open_subscribe = False
+open_subscribe = True
 subscribe_urls = [
     "https://m3u.ibert.me/txt/fmml_dv6.txt",
     "https://m3u.ibert.me/txt/o_cn.txt",
     "https://m3u.ibert.me/txt/j_iptv.txt",
 ]
-open_multicast = False
+open_multicast = True
 region_list = ["all"]
